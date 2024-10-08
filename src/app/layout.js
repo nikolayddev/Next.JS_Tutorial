72c93f104b6e04f824f94f28dae6cd06c7550796
nikolayddev <122819108+nikolayddev@users.noreply.github.com>
2024-10-08 17:58:09 +0300

    Created server actions to add and delete posts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
 import "./globals.css";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./fonts.css";
 
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
@@ -23,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <div className="container">
           <Navbar />
           {children}
